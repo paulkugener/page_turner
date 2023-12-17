dd595b276dae1291313db585c1666bc55639b4d8
paulkugener <paulkugener@gmail.com>
2023-12-17 20:01:39 +0100

    init

---
+++ pedal.ino
@@ -0,0 +1,38 @@
+#include <BleKeyboard.h>
+
+#define BUTTON_UP 17
+#define BUTTON_DOWN 18
+// Variables will change:
+//int lastStateUp = HIGH; 
+int currentStateUp;
+//int lastStateDown = HIGH; 
+int currentStateDown;
+
+BleKeyboard bleKeyboard;
+
+void setup() {
+  //Serial.begin(115200);
+  //Serial.println("Starting BLE work!");picadilly
+  pinMode(BUTTON_UP, INPUT_PULLUP);
+  pinMode(BUTTON_DOWN, INPUT_PULLUP);
+  bleKeyboard.begin();
+  //Serial.println("setup done");
+}
+
+void loop() {
+  if(bleKeyboard.isConnected()) {
+    currentStateUp = digitalRead(BUTTON_UP); 
+    currentStateDown = digitalRead(BUTTON_DOWN);
+
+    if (currentStateUp == LOW) {
+      //Serial.println("Scroll Up");
+      bleKeyboard.write(218); // up button
+    }
+    if (currentStateDown == LOW) {
+      //Serial.println("Scroll Down");
+      bleKeyboard.write(217); // down button
+    }
+    bleKeyboard.releaseAll();
+  }
+  delay(100);
+}
